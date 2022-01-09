a2e71eb11319bb0fa817d489f3aad4f9ca3b5aa3
lizazav <liziktele2@gmail.com>
2022-01-09 15:48:24 +0700

    heroku

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('./db.json');
-const middlewares = jsonServer.defaults({
-  static: './build'
-});
-const PORT = process.env.PORT || 8000;
-server.use(middlewares);
-server.use(jsonServer.rewriter({
-  '/api/*': '/$1',
-}))
-server.use(router);
-server.listen(PORT, () => {
-  console.log('Server is running');
-});
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/sankeyproject-main'));
+
+app.get('/*', (req, res) =>
+  res.sendFile('index.html', {root: 'dist/sankeyproject-main/'}),
+);
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
