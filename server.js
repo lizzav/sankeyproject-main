16291a530023a5d2277649def7fceb9c6a108dac
lizazav <liziktele2@gmail.com>
2022-01-09 16:11:07 +0700

    heroku

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/sankeyproject-main'));
+app.use(express.static(__dirname + '/dist/sankey'));
 
-app.get('/*', (req, res) =>  res.sendFile(path.join(__dirname+'/dist/sankeyproject-main/index.html'))
+app.get('/*', (req, res) =>  res.sendFile(path.join(__dirname+'/dist/sankey/index.html'))
 );
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(8080);
